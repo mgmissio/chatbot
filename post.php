329f2bc95b740a07f1ad8297059d2fbd2cd17a23
mgmissio <136394005+mgmissio@users.noreply.github.com>
2024-06-17 22:11:55 -0300

    Add files via upload

---
+++ post.php
@@ -0,0 +1,7 @@
+<?php
+include __DIR__.'../../../vendor/autoload.php';
+use Tigo\ChatterBot\Response;
+use Tigo\ChatterBot\Bot\BotUniversal;
+
+$response  = new Response(new BotUniversal);
+echo $response->check($_POST['msg']);
